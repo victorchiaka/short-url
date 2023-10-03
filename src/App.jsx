82e5c62fbf77f47a59d1779fec4cfb3177d36a77
Victor Chiaka <chiakavictor205@gmail.com>
2023-10-03 19:16:01 +0100

    changed site title

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import Title from "./components/title";
 import Form from "./components/form";
+import { useEffect } from "react";
 
 const App = () => {
+  useEffect(() => {
+    document.title = "Short Urls";
+  }, []);
+
   return (
     <>
       <div className=" bg-soothblack w-[100%] h-[100%]">
